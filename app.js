f18f9f00781567d97b80e87f19542f8b0d16aa1c
edio <dev.edioj@gmail.com>
2022-02-02 08:12:02 -0300

    middlewares and crud

--- app.js
+++ app.js
@@ -2,24 +2,32 @@ var express = require('express'),
   load	=	require('express-load'),
 	bodyParser	=	require('body-parser'),
 	cookieParser	=	require('cookie-parser'),
-	expressSession	=	require('express-session'),
+  expressSession = require('express-session'),
+  methodoOverride = require('method-override'),
   load = require('express-load'),
-  app = express();
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(cookieParser('ntalk'));
-app.use(expressSession());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:	true}));
-app.use(express.static(__dirname + '/public'));
+  error = require('./middleware/error'),
+  app = express(),
+  server = require('http').Server(app),
+  io = require('socket.io')(server)
 
 load('models')
   .then('controllers')
   .then('routes')
   .into(app);
 
-app.listen(3000, function () {
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(methodoOverride('_method'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:	true}));
+app.use(express.static(__dirname + '/public'));
+app.use(error.notFound);
+app.use(error.serverError);
+
+
+server.listen(3000, function () {
   console.log('Ntalk no ar.');
 });
 
--- routes/contatos.js
+++ routes/contatos.js
@@ -1,6 +1,15 @@
+const { application } = require("express");
+
 module.exports = function (app) {
-    var contatos = app.controllers.contatos;
-    app.get('/contatos', contatos.index);
+    var autenticar = require('./../middleware/autenticador'),
+        contatos = app.controllers.contatos;
+    
+    app.get('/contatos', autenticar, contatos.index);
+    app.get('/contatos/:id', autenticar, contatos.show);
+    app.post('/contatos', autenticar, contatos.create);
+    app.get('/contato/:id/editar', autenticar, contatos.edit);
+    app.put('/contato/:id', autenticar, contatos.update);
+    app.delete('/contato/:id', autenticar, contatos.destroy);
 };
 
 
