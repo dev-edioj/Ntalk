7d4fffb92648b0b719c09159e612213056339907
edio <dev.edioj@gmail.com>
2022-02-05 18:10:57 -0300

    resolve views

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -1,32 +1,32 @@
 module.exports = function (app) {
     var ContatoController = {
         index: function (req, res) {
-            var usuario = req.session.usuario,
-                 contatos = usuario.contatos,
-                params = {  usuario: usuario,
+            var usuario = req.session.usuario;
+            var contatos = usuario.contatos;
+            var params = {  usuario: usuario,
                            contatos: contatos };
             res.render('contatos/index', params);
         },
 
         create: function (req, res) {
-            var contato = req.body.contato,
-                usuario = req.session.usuario;
+            var contato = req.body.contato;
+            var usuario = req.session.usuario;
             usuario.contatos.push(contato);
             res.redirect('/contatos');
         },
 
         show: function (req, res) {
-            var id = req.params.id,
-                contato = req.session.usuario.contatos[id],
-                params = { contato: contato,
+            var id = req.params.id;
+            var contato = req.session.usuario.contatos[id];
+            var  params = { contato: contato,
                            id: id };
             res.render('contatos/show', params);
         },
 
         edit: function (req, res) {
-            var id = req.params.id,
-                contato = req.session.usuario.contatos[id],
-                params = {
+            var id = req.params.id;
+            var contato = req.session.usuario.contatos[id];
+            var params = {
                     usuario: usuario,
                     contato: contato,
                     id: id
